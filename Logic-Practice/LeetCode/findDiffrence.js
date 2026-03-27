65eb057efa546d962c4f36f15c8cd977975460ba
skepttalk <kuldeepsingh861915@gmail.com>
2026-03-27 21:04:49 +0530

    chore:added function verion to code

--- Logic-Practice/LeetCode/findDiffrence.js
+++ Logic-Practice/LeetCode/findDiffrence.js
@@ -14,3 +14,20 @@ for (let i = 0; i < t.length; i++) {
     }
     freq[t[i]]--;
 }
+
+
+
+// var findTheDifference = function(s, t) {
+//     let freq = {};
+
+//     for (let i = 0; i < s.length; i++) {
+//         freq[s[i]] = (freq[s[i]] || 0) + 1;
+//     }
+
+//     for (let i = 0; i < t.length; i++) {
+//         if (!freq[t[i]]) {
+//             return t[i];
+//         }
+//         freq[t[i]]--;
+//     }
+// };
