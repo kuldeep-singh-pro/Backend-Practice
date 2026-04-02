3416dfcb17007d61bbfb08f6d04f702fb6905223
skepttalk <kuldeepsingh861915@gmail.com>
2026-04-02 20:51:04 +0530

    feat:interview question setup files

---
+++ Logic-Practice/test.js
@@ -0,0 +1,33 @@
+    import express from "express";
+
+
+    const PORT = 5050;
+    let uid = 0;
+
+    const app = express();
+
+    app.use(express.json());
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       app.listen(PORT, () => {
+        console.log(`🚀 Server running on port ${PORT}`);
+    });
