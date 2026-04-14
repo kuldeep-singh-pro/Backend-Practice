5ee6a973b96b8e02ec5439cec5ad8c5912096107
skepttalk <kuldeepsingh861915@gmail.com>
2026-04-14 17:39:35 +0530

    feat(string):Leetcode day 44 first question multiply two string

---
+++ Logic-Practice/LeetCode/multiplyString.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+
+    if (num1 === "0" || num2 === "0") return "0";
+
+    let m = num1.length;
+    let n = num2.length;
+
+    let result = new Array(m + n).fill(0);
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+
+            let mul = (num1[i] - '0') * (num2[j] - '0');
+
+            let sum = mul + result[i + j + 1];
+
+            result[i + j + 1] = sum % 10;
+            result[i + j] += Math.floor(sum / 10);
+        }
+    }
+
+    while (result[0] === 0) {
+        result.shift();
+    }
+
+    return result.join('');
+};
