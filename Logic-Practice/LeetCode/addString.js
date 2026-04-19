0ab54776c7cf4353b2e7915d9a06d4981450ebff
skepttalk <kuldeepsingh861915@gmail.com>
2026-04-19 15:14:19 +0530

    feat(string):leetcode day 48 firwst question add two string

---
+++ Logic-Practice/LeetCode/addString.js
@@ -0,0 +1,21 @@
+var addStrings = function(num1, num2) {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let carry = 0;
+    let result = [];
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let digit1 = i >= 0 ? num1[i] - '0' : 0;
+        let digit2 = j >= 0 ? num2[j] - '0' : 0;
+
+        let sum = digit1 + digit2 + carry;
+
+        result.push(sum % 10);
+        carry = Math.floor(sum / 10);
+
+        i--;
+        j--;
+    }
+
+    return result.reverse().join('');
+};
