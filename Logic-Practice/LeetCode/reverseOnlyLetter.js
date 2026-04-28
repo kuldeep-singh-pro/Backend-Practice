cbdd253e4d69a78b60fa9b7b33d5c4d47627c3ec
skepttalk <kuldeepsingh861915@gmail.com>
2026-04-28 19:21:00 +0530

    fix(string):leetcode day 58 first question reverse only letter from string

--- Logic-Practice/LeetCode/reverseOnlyLetter.js
+++ Logic-Practice/LeetCode/reverseOnlyLetter.js
@@ -1,27 +1,36 @@
-let s = "ab-cd";
 
-let arr = s.split(""); 
+var reverseOnlyLetters = function(s) {
 
-let left = 0;
-let right = arr.length - 1;
+    let arr = s.split("");
 
-while (left < right) {
+    let left = 0;
+    let right = arr.length - 1;
 
-    if (arr[left].charCodeAt(0) < 33 || arr[left].charCodeAt(0) > 122) {
+    const isLetter = (ch) => {
+        let code = ch.charCodeAt(0);
+        return (
+            (code >= 65 && code <= 90) || 
+            (code >= 97 && code <= 122)
+        );
+    };
+
+    while (left < right) {
+
+        if (!isLetter(arr[left])) {
+            left++;
+            continue;
+        }
+
+        if (!isLetter(arr[right])) {
+            right--;
+            continue;
+        }
+
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+
         left++;
-        continue;
-    }
-
-    if (arr[right].charCodeAt(0) < 33 || arr[right].charCodeAt(0) > 122) {
         right--;
-        continue;
     }
 
-
-    [arr[left], arr[right]] = [arr[right], arr[left]];
-
-    left++;
-    right--;
-}
-
-console.log(arr.join(""));
+    return arr.join("");
+};
