fcb19a8814c13853005430907287f1d6faedb0ad
skepttalk <kuldeepsingh861915@gmail.com>
2026-06-11 21:10:52 +0530

    feat(array):leetcode day 68 sort color in asceending order

---
+++ Logic-Practice/LeetCode/sortColor.js
@@ -0,0 +1,11 @@
+var sortColors = function(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = 0; j < nums.length - 1 - i; j++) {
+            if (nums[j] > nums[j + 1]) {
+                let temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+};
